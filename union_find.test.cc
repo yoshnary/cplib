cc2e4f4678844767d1f024562b4efacd7e5fc1c0
yoshnary <yoshinari.nozomu@gmail.com>
2020-05-08 22:46:29 +0900

    Add UnionFind

---
+++ union_find.test.cc
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "union_find.h"
+#include <iostream>
+
+int main() {
+    int n, q; std::cin >> n >> q;
+    UnionFind uf(n);
+    for (int i = 0; i < q; i++) {
+        int t, u, v; std::cin >> t >> u >> v;
+        if (t) std::cout << uf.same(u, v) << std::endl;
+        else uf.unite(u, v);
+    }
+    return 0;
+}
