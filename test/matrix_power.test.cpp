ae2a32280d8a84b14c81fe4016cb85af51b5eb7d
yoshnary <yoshinari.nozomu@gmail.com>
2020-05-09 04:31:48 +0900

    Add Matrix Power

---
+++ test/matrix_power.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1050"
+
+#include "../lib/matrix_power.hpp"
+#include "../lib/modint.hpp"
+#include <iostream>
+
+int main() {
+    int m, k; std::cin >> m >> k;
+    Matrix<Mint> mat(m), v(m, 1);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            mat[(j + i) % m][j]++;
+            mat[(j * i) % m][j]++;
+        }
+    }
+    v[0][0] = 1;
+    std::cout << matmul(matpow(mat, k), v)[0][0] << std::endl;
+    return 0;
+}
