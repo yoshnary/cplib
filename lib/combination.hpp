b3ee165587433103c6c11677b8e6718215d43dc6
yoshnary <yoshinari.nozomu@gmail.com>
2020-05-13 18:08:04 +0900

    Fix include guard

--- lib/combination.hpp
+++ lib/combination.hpp
@@ -1,5 +1,5 @@
-#ifndef CPLIB_LIB_COMBINATORICS_H_
-#define CPLIB_LIB_COMBINATORICS_H_
+#ifndef CPLIB_LIB_COMBINATION_H_
+#define CPLIB_LIB_COMBINATION_H_
 
 #include "../lib/modint.hpp"
 #include <vector>
@@ -36,4 +36,4 @@ Mint H(int a, int b) {
     return C(a + b - 1, b);
 }
 
-#endif  // CPLIB_LIB_COMBINATORICS_H_
+#endif  // CPLIB_LIB_COMBINATION_H_
