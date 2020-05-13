5c00b327fd89b12b918db112aaf3ade5b4f897fa
yoshnary <yoshinari.nozomu@gmail.com>
2020-05-13 09:57:12 +0900

    Fix variable name

--- test/rolling_hash.aoj.2444.test.cpp
+++ test/rolling_hash.aoj.2444.test.cpp
@@ -1,4 +1,4 @@
-#define PROBlEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2444"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2444"
 
 #include <iostream>
 #include <string>
